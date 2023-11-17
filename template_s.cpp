ad22a51d3f9653aa3998ed4263c34beaa02c4dd0
27j4 <pandeyanshul2004@gmail.com>
2023-11-17 13:37:18 +0530

    FIBONACCI using matrix exponentiation

--- template_s.cpp
+++ template_s.cpp
@@ -143,3 +143,103 @@ int cnt_power(int n,int x){
 	}
 }
 
+// Template 11 - Matrix Expo Ques[Fibbonaci]
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define pb emplace_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define vll vector<ll>
+#define all(x) (x).begin(), (x).end()
+#define loop(i,a,b) for(ll i=a;i<b;i++)
+#define rloop(i,b,a) for(ll i=b;i>=a;i--)
+#define test int t; cin>>t; while(t--)
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<":"; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*--------------------------------------------------------------------------------------------------------------*/
+ll arr[3];
+ll I[3][3] , T[3][3];
+void mul(ll A[3][3] , ll B[3][3] , ll dim){
+    ll res[dim+1][dim+1];
+    for(ll i = 1 ; i <= dim ;i++){
+        for(ll j = 1 ;j <= dim ;j++){
+            res[i][j] = 0;
+            for(ll k = 1 ; k <= dim ;k++){
+                res[i][j]+=(A[i][k]*B[k][j])%mod;
+            }
+        }
+    }
+    for(int i = 1;i <= dim ;i++){
+        for(int j = 1;j <= dim ;j++){
+            A[i][j] = res[i][j];
+        }
+    }
+}
+ll getfib(ll n ){
+    if(n==0){
+        return arr[1];
+    }
+    if(n==1){
+        return arr[2];
+    }
+    I[1][1] = 1 , I[2][2] = 1;
+    I[1][2] = I[2][1] = 0;
+    T[1][2] = T[2][1] = T[2][2] = 1;
+    T[1][1] = 0;
+    n = n-1;
+    while(n){
+        if(n&1){
+            mul(I,T,2);
+            n--;
+        }
+        else{
+            mul(T,T,2);
+            n = n / 2;
+        }
+    }
+    return (arr[1]*I[1][2] + arr[2]*I[2][2])%mod;
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("error.txt", "w", stderr);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t ;
+    cin >> t;
+    while(t--){
+        cin >> arr[1] >> arr[2];
+        ll n;
+        cin >> n;
+        cout << getfib(n) << "\n";
+    }
+}
+
+/*---Anshul_Pandey---------------------------------------------------------------------------------------------*/
+
