4d61f030c539de218f208442aa656a142413b4af
27j4 <pandeyanshul2004@gmail.com>
2023-11-24 15:01:16 +0530

    INVERSION COUNT

--- template_s.cpp
+++ template_s.cpp
@@ -1,39 +1,39 @@
-// Tenplate 1 -  Palindrome in a string 
-bool palin(string s){
-	if(s.size()<=1){
-		return true;
-	}
-	int i = 0 , j = s.size()-1;
-	while(i<j){
-		if(s[i]!=s[j]){
-			return false;
-		}
-		i++;
-		j--;
-	}
-	return true;
+
+
+/*-----------------------------Number Theory --------------------------------*/
+
+// 1. To finding GCD
+ll gcd(ll a , ll b){
+    if(b==0){
+        return a;
+    }
+    else{
+        return gcd(b,a%b);
+    }
 }
 
 
-// Template 2 - Binary exponentiation
+
+// 2. Binary exponentiation
 const int m = 1e9+7;
 ll binpow(ll a , ll b){
-	ll ans = 1;
-	while(b){
-		if(b&1){
-			ans = (ans*a)%m;
-			b--;
-		}
-		else{
-			a = (a*a)%m;
-			b = b/2;
-		}
-	}
-	return ans;
+    ll ans = 1;
+    while(b){
+        if(b&1){
+            ans = (ans*a)%m;
+            b--;
+        }
+        else{
+            a = (a*a)%m;
+            b = b/2;
+        }
+    }
+    return ans;
 }
 
 
-// Template 3 - Factorisation
+
+// 3. Factorisation
 const int maxN = 10000000;
 int arr[maxN+1]={0};
 void factorization(){
@@ -66,7 +66,8 @@ map<int,int> getFactorization(int x)
 }
 
 
-// Template 4 - Seive
+
+// 4. Seive - [ To store all the prime numbers]
 const int maxN = 10000000;
 bool arr[maxN+1]={0};
 void seive(){
@@ -88,100 +89,35 @@ void seive(){
 }
 
 
-// Template 5 - ETF[Euler Tuotient Function]
+
+// 5. ETF
 const int maxN = 1e7;
 int phi[maxN+1];
 void etf(){
-	// Note -- call etf , take according size of array
-	for(int i = 1 ; i <= maxN ;i++){
-		phi[i] = i-1;
-	}
-	for(int i = 2;i<= maxN;i++){
-		if(phi[i]==i-1){
-			for(int j = i*2 ;j<= maxN ;j+=i){
-				phi[j] = phi[j] - phi[j]/i;
-			}
-		}
-	}
-}
-
-// Template 6 - GCD
-int gcd(int a , int b){
-	if(b==0){
-		return a;
-	}
-	else{
-		return gcd(b,a%b);
-	}
+    // Note -- call etf , take according size of array
+    for(int i = 1 ; i <= maxN ;i++){
+        phi[i] = i-1;
+    }
+    for(int i = 2;i<= maxN;i++){
+        if(phi[i]==i-1){
+            for(int j = i*2 ;j<= maxN ;j+=i){
+                phi[j] = phi[j] - phi[j]/i;
+            }
+        }
+    }
 }
 
 
-// Template 7 - Matrix Multiplication
-// Template 8 - MEX[Minimum Excluded]
-// Template 9 - Prefix , Suffix Array
-int prefix[n];
-prefix[0] = arr[0];
-for(int i =1;i < n;i++){
-	prefix[i] = arr[i] + prefix[i-1]; // as in question
-}
 
-int suffix[n];
-suffix[n-1] = arr[n-1];
-
-// Temaplate 10 - To find how many powers of x present in n
-int cnt_power(int n,int x){
-	if(n==0){
-		return 0;
-	}
-	else{
-		int cnt = 0;
-		while(!(n%x)){
-			cnt++;
-			n = n/x;
-		}
-		return cnt;
-	}
-}
-
-// Template 11 - Matrix Expo Ques[Fibbonaci]
+// 6. Question of matrix exponentiation [Recurrence relation]
 #include<bits/stdc++.h>
 using namespace std;
 #define mod 1000000007
-#define pb emplace_back
-#define ff first
-#define ss second
-#define vi vector<int>
-#define vll vector<ll>
-#define all(x) (x).begin(), (x).end()
-#define loop(i,a,b) for(ll i=a;i<b;i++)
-#define rloop(i,b,a) for(ll i=b;i>=a;i--)
-#define test int t; cin>>t; while(t--)
+
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double lld;
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x <<":"; _print(x); cerr << endl;
-#else
-#define debug(x)
-#endif
-void _print(ll t) {cerr << t;}
-void _print(int t) {cerr << t;}
-void _print(string t) {cerr << t;}
-void _print(char t) {cerr << t;}
-void _print(lld t) {cerr << t;}
-void _print(double t) {cerr << t;}
-void _print(ull t) {cerr << t;}
-template <class T, class V> void _print(pair <T, V> p);
-template <class T> void _print(vector <T> v);
-template <class T> void _print(set <T> v);
-template <class T, class V> void _print(map <T, V> v);
-template <class T> void _print(multiset <T> v);
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-/*--------------------------------------------------------------------------------------------------------------*/
+
 ll arr[3];
 ll I[3][3] , T[3][3];
 void mul(ll A[3][3] , ll B[3][3] , ll dim){
@@ -241,5 +177,183 @@ int main()
     }
 }
 
-/*---Anshul_Pandey---------------------------------------------------------------------------------------------*/
+
+
+// 7. For calculating binomial coefficient
+int fac[1000001];
+const int m = 1000000007;
+int power(int b , int p){
+    // condition of odd p
+    int result =1;
+    while(p){
+        if(p&1){
+            result = (result * 1LL*b)%m;
+            p--;
+        }
+        // condition of even p
+        else{
+            p = p/2;
+            b = (b*1LL*b)%m;
+        }
+    }
+    return result;
+}
+ 
+int c(int a, int b){
+    if (a<b){
+        return -1;
+    }
+    else{
+        ll res = fac[a];
+        res = (res*1LL*power(fac[b],m-2))%m; // BE
+        res = (res*1LL*power(fac[a-b],m-2))%m; // BE
+        return res;
+    }
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("error.txt", "w", stderr);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // to find aCb
+ 
+   // 1. precomputing a!% m
+    fac[0]=1;
+    fac[1]=1;
+    for(int i = 2; i<= 1000000;i++){
+        fac[i] = (fac[i-1]*1LL*i)%m; // 1LL -> for not having integer overflow
+    }
+ 
+    int t;
+    cin >> t;
+    while(t--){
+        int a , b;
+        cin >> a >> b;
+        cout << c(a,b) << '\n';
+    }
+}
+
+/* INVERSION COUNT USING MERGE SORT */
+
+// C++ program for Merge Sort
+#include <bits/stdc++.h>
+using namespace std;
+
+// Merges two subarrays of array[].
+// First subarray is arr[begin..mid]
+// Second subarray is arr[mid+1..end]
+void merge(int array[], int const left, int const mid,
+        int const right ,vector<int> &vec)
+{
+    int const subArrayOne = mid - left + 1;
+    int const subArrayTwo = right - mid;
+
+    // Create temp arrays
+    auto *leftArray = new int[subArrayOne],
+        *rightArray = new int[subArrayTwo];
+
+    // Copy data to temp arrays leftArray[] and rightArray[]
+    for (auto i = 0; i < subArrayOne; i++)
+        leftArray[i] = array[left + i];
+    for (auto j = 0; j < subArrayTwo; j++)
+        rightArray[j] = array[mid + 1 + j];
+
+    auto indexOfSubArrayOne = 0, indexOfSubArrayTwo = 0;
+    int indexOfMergedArray = left;
+
+    // Merge the temp arrays back into array[left..right]
+    while (indexOfSubArrayOne < subArrayOne
+        && indexOfSubArrayTwo < subArrayTwo) {
+        if (leftArray[indexOfSubArrayOne]
+            <= rightArray[indexOfSubArrayTwo]) {
+            array[indexOfMergedArray]
+                = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+            vec[indexOfSubArrayOne] += indexOfSubArrayTwo;
+        }
+        else {
+            array[indexOfMergedArray]
+                = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+            // vec[indexOfMergedArray] += indexOfSubArrayTwo;
+        }
+        indexOfMergedArray++;
+    }
+
+    // Copy the remaining elements of
+    // left[], if there are any
+    while (indexOfSubArrayOne < subArrayOne) {
+        array[indexOfMergedArray]
+            = leftArray[indexOfSubArrayOne];
+        vec[indexOfSubArrayOne] += indexOfSubArrayTwo;
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+
+    // Copy the remaining elements of
+    // right[], if there are any
+    while (indexOfSubArrayTwo < subArrayTwo) {
+        array[indexOfMergedArray]
+            = rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
+    delete[] leftArray;
+    delete[] rightArray;
+}
+
+// begin is for left index and end is right index
+// of the sub-array of arr to be sorted
+void mergeSort(int array[], int const begin, int const end ,vector<int> &vec)
+{
+    if (begin >= end)
+        return;
+
+    int mid = begin + (end - begin) / 2;
+    mergeSort(array, begin, mid,vec);
+    mergeSort(array, mid + 1, end,vec);
+    merge(array, begin, mid, end,vec);
+}
+
+// Driver code
+int main()
+{
+    int arr[] = {1,5,4,5,6};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Given array is \n";
+    vector<int> vec(arr_size,0);
+    mergeSort(arr, 0, arr_size - 1,vec);
+    for(int i = 0 ;i < arr_size ;i++){
+        cout << vec[i] << " ";
+    }
+    cout << "\n";
+    return 0;
+}
+
+
+
+
+
+/*-------------------------------Subarray_Problems-------------------------------*/
+
+
+// 1. Kadanes Algorithm
+ll maxSubArraySum(ll a[], ll start , ll end)
+{
+    ll max_so_far = LLONG_MIN, max_ending_here = 0;
+ 
+    for (ll i = start; i <= end; i++) {
+        max_ending_here = max_ending_here + a[i];
+        if (max_so_far < max_ending_here)
+            max_so_far = max_ending_here;
+ 
+        if (max_ending_here < 0)
+            max_ending_here = 0;
+    }
+    return max_so_far;
+}
+
 
